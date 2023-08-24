06925530ce407187346416a4a6cb6e47da1d39a6
cerciber <cerciber@hotmail.com>
2023-08-24 18:40:20 -0500

    feat: Generalize validations by status code

--- src/adapters/controllers/user/userController.js
+++ src/adapters/controllers/user/userController.js
@@ -9,44 +9,77 @@ const {
 } = require('@src/application/userCases');
 const {
   validate,
+  validateByStatus,
 } = require('@src/adapters/controllers/validation/validationController');
 
 // List data
 async function getUserslistController() {
-  return getUserslistCase();
+  // Apply bussiness logic
+  const usersResponse = await getUserslistCase();
+
+  // Validate output
+  const outputValidation = validateByStatus(usersResponse.status, {
+    200: [
+      ['validateResponse', [200, usersResponse]],
+      ['validateType', ['object', usersResponse.body]],
+      ['validateSchema', ['Users', usersResponse.body.users]],
+    ],
+  });
+
+  // Return incorrect validation output
+  if (!outputValidation.valid) {
+    return response.error(400, outputValidation.badMessage, {
+      errors: outputValidation.errors,
+    });
+  }
+
+  // Return correct validation output
+  return usersResponse;
 }
 
 // Get by id
-async function getUserByIdController(id) {
+async function getUserByIdController(params) {
+  // Get input
+  const { id } = params;
+
+  // Validate input
+  const inputValidation = validate([
+    ['validateType', ['string', id]],
+    ['validateNonEmptyString', [id]],
+  ]);
+
+  // Return incorrect validation input
+  if (!inputValidation.valid) {
+    return response.error(400, inputValidation.badMessage, {
+      errors: inputValidation.errors,
+    });
+  }
+
   // Apply bussiness logic
   const userResponse = await getUserByIdCase(id);
 
-  // Validate responses
-  if (userResponse.status === 200) {
-    const responseValidation = validate([
+  // Validate output
+  const outputValidation = validateByStatus(userResponse.status, {
+    200: [
       ['validateResponse', [200, userResponse]],
       ['validateType', ['object', userResponse.body]],
       ['validateSchema', ['User', userResponse.body.user]],
-    ]);
-    if (!responseValidation.valid) {
-      return response.error(400, responseValidation.errors[0].message, {
-        errors: responseValidation.errors,
-      });
-    }
-  } else if (userResponse.status === 404) {
-    const responseValidation = validate([
+    ],
+    404: [
       ['validateResponse', [404, userResponse]],
       ['validateType', ['object', userResponse.body]],
       ['validateEmptyObject', [userResponse.body]],
-    ]);
-    if (!responseValidation.valid) {
-      return response.error(400, responseValidation.errors[0].message, {
-        errors: responseValidation.errors,
-      });
-    }
+    ],
+  });
+
+  // Return incorrect validation output
+  if (!outputValidation.valid) {
+    return response.error(400, outputValidation.badMessage, {
+      errors: outputValidation.errors,
+    });
   }
 
-  // Send result
+  // Return correct validation output
   return userResponse;
 }
 
