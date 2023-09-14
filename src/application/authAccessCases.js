1d10d6c587b280c2d8b6aa382945b73290bb1f0f
cerciber <cerciber@hotmail.com>
2023-09-14 16:07:58 -0500

    feat: Auth access rol validation advances

--- src/application/authAccessCases.js
+++ src/application/authAccessCases.js
@@ -2,6 +2,10 @@
 const response = require('@src/adapters/presenters/response');
 const Auth = require('@src/entities/Auth');
 const paths = require('@src/utils/statics/paths');
+const gateway = require('@src/adapters/gateways/fakeDBGateway/fakeDBGateway');
+
+// Define table users auth
+const TABLE = 'authUsers';
 
 // validate Public path case
 async function getPathDataCase(path) {
@@ -35,7 +39,47 @@ async function validateAccessCase(basePath, method, token) {
     return response.success(401, 'Access to this resource not allowed.', {});
   }
 
+  // Check if token have id
+  if (!payload.id) {
+    // Return response
+    return response.success(401, 'Access to this resource not allowed.', {});
+  }
+
+  // Get gateway data
+  const gatewayUserAuth = await gateway.getByAttributes(TABLE, {
+    id: payload.id,
+  });
+
+  // Check if user was found
+  if (!gatewayUserAuth || gatewayUserAuth.length === 0) {
+    // Return response
+    return response.success(401, 'Access to this resource not allowed.', {});
+  }
+
+  // Get authorization
+  const authorization = gatewayUserAuth[0]?.authorization;
+
+  // Check if user has authorization configuration
+  if (
+    !(
+      authorization?.type ||
+      !(
+        authorization?.type === 'rol' && typeof authorization?.rol === 'string'
+      ) ||
+      !(
+        authorization?.type === 'custom' &&
+        Array.isArray(authorization?.permissions)
+      )
+    )
+  ) {
+    // Return response
+    return response.success(401, 'Access to this resource not allowed.', {});
+  }
+
+  // If rol, get rol authorization configuration
+
   // Check if user have authorization to this request
+  console.log(gatewayUserAuth);
 
   // Return response
   return response.success(200, 'Access to this resource allowed.', {
